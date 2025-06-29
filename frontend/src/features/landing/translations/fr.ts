da0c152c3fbf52cd0826cf46626b994247514834
tentacode <gabriel+github@tentacode.dev>
2025-06-29 16:23:24 +0200

    🔥 Cleanup frontend to start from scratch

--- frontend/src/features/landing/translations/fr.ts
+++ frontend/src/features/landing/translations/fr.ts
@@ -1,8 +1,4 @@
 export default {
   "title": "Oh! Dit",
   "subtitle": "Outil d'Audit d'Accessibilité Web",
-  "comeBackSoon": "Revenez bientôt",
-  "description": "Nous construisons quelque chose d'extraordinaire pour les professionnels de l'accessibilité.",
-  "rgaaCompliance": "La conformité RGAA 4.1 simplifiée.",
-  "inDevelopment": "En développement"
 } as const; 
