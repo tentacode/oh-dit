19f73466fd4ec21195a6bb171d5ebc6158f1ad37
tentacode <gabriel+github@tentacode.dev>
2025-12-21 10:17:41 +0100

    :lipstick: Better skeleton on project data loading

--- app/src/features/project/data_providers/ProjectDataProvider.tsx
+++ app/src/features/project/data_providers/ProjectDataProvider.tsx
@@ -3,7 +3,6 @@ import { useAuditStore } from "../../audit/store/auditStore";
 import { useFetchProjectCompliances } from "../../compliance/queries/useFetchProjectCompliances";
 import { useFetchRuleSet } from "../../rule_set/queries/useFetchRuleSet";
 import { useFetchProject } from "../queries/useFetchProject";
-import CardSkeleton from "@/src/components/skeleton/CardSkeleton";
 import ErrorBox from "../../error_handling/components/ErrorBox";
 import { notFound } from "next/navigation";
 import { useFetchProjectIssues } from "../../issue/queries/useFetchProjectIssues";
@@ -21,7 +20,7 @@ export default function ProjectDataProvider({
     isLoading: isLoadingProject,
     isError: isErrorProject,
   } = useFetchProject(projectUuid);
-  
+
   const {
     data: projectCompliances,
     isLoading: isLoadingCompliances,
@@ -42,12 +41,15 @@ export default function ProjectDataProvider({
     isError: isErrorIssues,
   } = useFetchProjectIssues(projectUuid);
 
-  const { setProject, setCompliances, setRuleSet, setIssues } =
-    useAuditStore();
+  const { setProject, setCompliances, setRuleSet, setIssues } = useAuditStore();
 
-  const getProjectSetting = useAuditSettingsStore((state) => state.getProjectSetting); 
+  const getProjectSetting = useAuditSettingsStore(
+    (state) => state.getProjectSetting
+  );
   const projectSetting = getProjectSetting(projectUuid);
-  const setProjectSetting = useAuditSettingsStore((state) => state.setProjectSetting);
+  const setProjectSetting = useAuditSettingsStore(
+    (state) => state.setProjectSetting
+  );
 
   useEffect(() => {
     if (project) {
@@ -56,11 +58,20 @@ export default function ProjectDataProvider({
       if (!projectSetting.currentScreenUuid) {
         const firstScreenUuid = project.screens[0]?.uuid || null;
         if (firstScreenUuid) {
-          setProjectSetting({ projectUuid, currentScreenUuid: firstScreenUuid });
+          setProjectSetting({
+            projectUuid,
+            currentScreenUuid: firstScreenUuid,
+          });
         }
       }
     }
-  }, [project, setProject, projectSetting.currentScreenUuid, setProjectSetting, projectUuid]);
+  }, [
+    project,
+    setProject,
+    projectSetting.currentScreenUuid,
+    setProjectSetting,
+    projectUuid,
+  ]);
 
   useEffect(() => {
     if (ruleSet) {
@@ -80,8 +91,26 @@ export default function ProjectDataProvider({
     }
   }, [projectIssues, setIssues]);
 
-  if (isLoadingProject || isLoadingCompliances || isLoadingRuleSet || isLoadingIssues)
-    return <CardSkeleton />;
+  if (
+    isLoadingProject ||
+    isLoadingCompliances ||
+    isLoadingRuleSet ||
+    isLoadingIssues
+  )
+    return (
+      <div className="horizontalGutter mb-5">
+        <h1 className={"h1 mb-4"}>Chargement du projet…</h1>
+        <div className="animate-pulse">
+        <div className="flex gap-3 mb-4" style={{width: '60%', height: '80px',}}>
+          <div className="h-5" style={{background: '#ffece6', height: '80px', flex: 1, borderRadius: '10px'}}></div>
+          <div className="h-5" style={{background: '#ffece6', height: '80px', flex: 1, borderRadius: '10px'}}></div>
+          <div className="h-5" style={{background: '#ffece6', height: '80px', flex: 1, borderRadius: '10px'}}></div>
+        </div>
+        <div className="h-5 w-4/6 mb-4" style={{background: '#ffece6', borderRadius: '10px'}}></div>
+        <div className="h-10 w-3/4" style={{background: '#ffece6', borderRadius: '10px', height: '200px'}}></div>
+        </div>
+      </div>
+    );
 
   if (isErrorProject || isErrorCompliances || isErrorRuleSet || isErrorIssues)
     return (
