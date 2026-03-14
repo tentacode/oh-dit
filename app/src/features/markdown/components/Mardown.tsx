6f6a53f4fb0f034b98224ceab340c05c7cabf44c
tentacode <gabriel+github@tentacode.dev>
2026-03-14 09:22:23 +0100

    :bug: Fix inline code in issue's markdown

--- app/src/features/markdown/components/Mardown.tsx
+++ app/src/features/markdown/components/Mardown.tsx
@@ -13,19 +13,40 @@ export default function Markdown({
   minimalHeadingLevel?: number;
 }) {
   return (
-      <MarkdownToJsx
-        className={className || ""}
-        options={{ overrides: { 
+    <MarkdownToJsx
+      className={className || ""}
+      options={{
+        ignoreHTMLBlocks: true,
+        overrides: {
           code: MarkdownCode,
-          h1: { component: MarkdownHeadingTag, props: { level: 1, minimalLevel: minimalHeadingLevel || 1 } },
-          h2: { component: MarkdownHeadingTag, props: { level: 2, minimalLevel: minimalHeadingLevel || 1 } },
-          h3: { component: MarkdownHeadingTag, props: { level: 3, minimalLevel: minimalHeadingLevel || 1 } },
-          h4: { component: MarkdownHeadingTag, props: { level: 4, minimalLevel: minimalHeadingLevel || 1 } },
-          h5: { component: MarkdownHeadingTag, props: { level: 5, minimalLevel: minimalHeadingLevel || 1 } },
-          h6: { component: MarkdownHeadingTag, props: { level: 6, minimalLevel: minimalHeadingLevel || 1 } },
-        } }}
-      >
-        {children}
-      </MarkdownToJsx>
+          h1: {
+            component: MarkdownHeadingTag,
+            props: { level: 1, minimalLevel: minimalHeadingLevel || 1 },
+          },
+          h2: {
+            component: MarkdownHeadingTag,
+            props: { level: 2, minimalLevel: minimalHeadingLevel || 1 },
+          },
+          h3: {
+            component: MarkdownHeadingTag,
+            props: { level: 3, minimalLevel: minimalHeadingLevel || 1 },
+          },
+          h4: {
+            component: MarkdownHeadingTag,
+            props: { level: 4, minimalLevel: minimalHeadingLevel || 1 },
+          },
+          h5: {
+            component: MarkdownHeadingTag,
+            props: { level: 5, minimalLevel: minimalHeadingLevel || 1 },
+          },
+          h6: {
+            component: MarkdownHeadingTag,
+            props: { level: 6, minimalLevel: minimalHeadingLevel || 1 },
+          },
+        },
+      }}
+    >
+      {children}
+    </MarkdownToJsx>
   );
 }
